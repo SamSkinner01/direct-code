056cc1286081b55802268a48637ec014651d0403
sam <saskinner19@gmail.com>
2022-11-26 10:07:15 -0500

    Added confirm modal to delete board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,12 +1,28 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Modal  from 'react-bootstrap/Modal';
+import {useState} from 'react';
 
 function Board(props) {
 
+    const [modalShow, setModalShow] = useState(false);
+    
     function deleteFN(){
         props.deleteFromDB(props.boardID);
+    }
+
+    const onShow = () => {
+        setModalShow(true);
+    }
+
+    const onHide = () => {
+        setModalShow(false);
+    }
+
+    const onHideConfirm = () => {
+        setModalShow(false);
+        deleteFN();
     }
 
   return (
@@ -19,7 +35,30 @@ function Board(props) {
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     
-      <Button variant="danger" onClick={deleteFN}>Delete</Button>
+      <Button variant="danger" onClick={onShow}>Delete</Button>
+
+      <Modal 
+      show={modalShow} 
+      onHide={onHide}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Delete A Board
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>
+          Selecting confirm will delete the board. This action cannot be undone.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={onHideConfirm}>Confirm</Button>
+      </Modal.Footer>
+    </Modal>
+
     </Card>
   );
 }
